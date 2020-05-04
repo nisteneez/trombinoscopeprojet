269139b5ae871fd0371953e34ec62324e091b72a
nisteneez <00siteweb00@gmail.com>
2020-05-04 13:50:24 +0200

    commit

---
+++ Site étudiant/index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inscription</title>
+	<link rel="shortcut icon" href="ucp2.jpg" type="image/x-icon">
+	<link rel="stylesheet" type="text/css" href="tb.css" >
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+</head> 
+<body>
+
+	<div id="menu">
+            <ul>
+            	<li><img src="ucp.png" id="logo"></li>
+                <li><a href="cleAPI.php"><img src="cle.jpg" id="icon" title="Documentation Cle API"></a></li>
+                <li><a href="connexion.php"><img src="login.jpg" id="icon" title="Accès au compte etudiant"></a></li>
+            </ul>
+    </div>
+
+ <hr>
+
+	<h1>Compte étudiant : Inscription</h1>
+
+	<br><br>
+
+	<div id="bloc_page">
+		<br>
+		<nav>
+			<ul>
+				<li><a href="connexion.php">Connexion</a></li>
+				<li><a href="index.php">Inscription</a></li>
+			</ul>
+		</nav>
+		<br>
+		<hr>
+
+	<div align="center">
+		<br> 
+			
+	<form  method="get" action="index.php">
+
+		<table>
+			<td><label>Nom :</label></td>
+			<tr>
+				<td><label><input placeholder="Nom" type="text" name="nom" required /></label></td>
+			</tr>
+			<td><br><label>Prénom :  </label></td>
+			<tr>
+				<td><input placeholder="Prénom" type="text" name="prenom" required /></td>
+			</tr>
+			<td><br><label>Adresse email :</label></td>
+			<tr>
+				<td><input placeholder=" Votre email"  type="email" name="email" required /></td>
+			</tr>
+			<td><br><label>Mot de passe :</label></td>
+			<tr>
+				<td><input placeholder="Votre mot de passe" type="password" name="password" required /></td>
+			</tr>
+			<br>	
+		</table>
+		<br>
+
+		<select name="filiere" id="filiere">
+			<option>Filière</option>
+			<option name="filiere">L1-MIPI</option>
+			<option name="filiere">L2-MIPI</option>
+			<option name="filiere">L3-I</option>
+			<option name="filiere">LP RS</option>
+			<option name="filiere">LPI-RIWS</option>
+		</select>
+
+		<select name="groupe" id="groupe">
+			<option>Groupe</option>
+			<option name="groupe">A</option>
+			<option name="groupe">B</option>
+			<option name="groupe">C</option>
+		</select>
+
+		<br><br><br>
+		<input type="submit" value="Je m'inscris" id="button"/>
+	</form>
+
+	<br>
+
+		<?php
+	function souscription(){
+	if (isset($_GET['email'])){
+	$contenu=file('tb.csv');
+	$found= FALSE;
+	for ($i=0; $i < sizeof($contenu) ; $i++) { 
+		$Clignes= explode(";", $contenu[$i]);
+		$Clignes[1]=str_replace("\n","",$Clignes[1]);
+		if ($_GET['email']==$Clignes[2] && md5($_GET['email'].$_GET['password'])==$Clignes[3]) {
+	        $found=TRUE;
+	    }
+	    
+	}
+	if ($found==TRUE){
+	    echo "Utilisateur déjà enregistré";
+	    
+	}
+	else{
+
+        $Fecriture= fopen("tb.csv", "a");
+		fwrite($Fecriture, $_GET['nom'].";".$_GET['prenom'].";".$_GET['email'].";".md5($_GET['email'].$_GET['password']).";".$_GET['filiere'].";".$_GET['groupe'].";"."\n");
+		fclose($Fecriture);
+		echo "Vous etes bien inscrit !";	  
+	     
+	} 
+	}
+	}
+	souscription();
+
+	?>
+
+	<br><br>
+
+	</div>
+	</div>
+
+	<br><br>
+
+	<hr>
+
+	<div id="footer">
+        <p>Projet Avril | Tout droit réservé | 2020</p>
+    </div>
+
+	<br><br><br>
+</body>
+</html>
---
+++ Site étudiant/information.php
@@ -0,0 +1,107 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mes informations</title>
+	<link rel="shortcut icon" href="ucp2.jpg" type="image/x-icon">
+	<link rel="stylesheet" type="text/css" href="tb.css" >
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+</head>
+<body>
+		<div id="menu">
+            <ul>
+            	<li><img src="ucp.png" id="logo"></li>
+                <li><a href="cleAPI.php"><img src="cle.jpg" id="icon" title="Documentation Cle API"></a></li>
+                <li><a href="connexion.php"><img src="login.jpg" id="icon" title="Accès au compte etudiant"></a></li>
+            </ul>
+    	</div>
+
+<hr>
+
+	<h1>Mes informations</h1>
+
+	<br>
+
+	<div id="bloc_page">
+		<br>
+		<nav>
+			<ul>
+				<li><a href="connexion.php">Connexion</a></li>
+				<li><a href="index.php">Inscription</a></li>
+			</ul>
+		</nav>
+		<br>
+		<hr>
+		<br>
+
+		<?php 
+			$informations=file("tb.csv");
+			$tab=array();
+			for ($i=0; $i < sizeof($informations) ; $i++) { 
+				$csv=explode(";", $informations[$i]);
+				if ($_SESSION['email']== $csv[2]){
+					$info=$csv;
+				}
+			}
+		?>
+
+		<form method="get" action="modification.php">
+		<table>
+			<tr>
+				<td><label id="labelinfo">Nom :</label></td>
+				<td><input value="<?php echo $info[0]; ?>" type="text" name="nom" id="inputinfo"/></td>
+			</tr>
+			<tr>
+				<td><br><br><label id="labelinfo">Prénom :</label></td>
+				<td><br><br><input value="<?php echo $info[1]; ?>" type="text" name="prenom" id="inputinfo"/></td>
+			</tr>
+			<tr>
+				<td><br><br><label id="labelinfo">E-mail :</label></td>
+				<td><br><br><input value="<?php echo $info[2]; ?>"  type="email" name="email" id="inputinfo"/></td>
+			</tr>
+			<tr>
+				<td><br><br><label id="labelinfo">Mot de passe : *</label></td>
+				<td><br><br><input value="<?php echo $info[3]; ?>"  type="password" name="password" id="inputinfo" title="Pour plus de sécurité le mot de passe est chiffré !" /></td>
+			</tr>
+			<tr>
+				<td><br><br><label id="labelinfo">Filière :</label></td>
+				<td><br><br><input value="<?php echo $info[4]; ?>"  type="text" name="filiere" id="inputinfo"/></td>
+			</tr>
+			<tr>
+				<td><br><br><label id="labelinfo">Groupe :</label></td>
+				<td><br><br><input value="<?php echo $info[5]; ?>"  type="text" name="groupe" id="inputinfo"/></td>
+			</tr>	
+		</table>
+
+		<br><br><br><br>
+
+		<input type="submit" name="enregistrer" value="Enregistrer" id="button"/><br>
+
+		</form>
+
+
+
+		<form method="post" action="deconnexion.php">
+			<input type="submit" value="Déconnexion" id="button"/>
+		</form>
+
+
+		<form method="post" enctype="multipart/form-data" action="photo.php">
+
+		<table>
+		<tr>
+			<td><label id="afficher">Photo :</label></td>
+			<td><br><input type="file" name="images" id="afficher"></td>
+			<td><input type="submit" value="Afficher" id="buttonphoto"></td>
+		</tr>	
+		</table>
+
+		<img id="photo" src="<?php echo './images/' . basename($info[2]."jpg");?>">
+		
+		</form>
+
+	</div>
+	
+</body>
+</html>
